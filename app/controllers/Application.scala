c8548b8ea09b6c5522ee6c39e7e22a6973bae9fa
Hiren.Hiranandani <hhiranan@gmail.com>
2014-03-13 17:10:32 -0400

    Added search, but broken.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,19 +4,28 @@ import play.api._
 import play.api.mvc._
 import com.typesafe.training.scalatrain.Station
 import com.typesafe.training.scalatrain.TestData
+import play.api.data.Form
 
 object Application extends Controller {
   import TestData.planner
 
+  def search = Action {
+    Ok(views.html.search(planner.stations))
+  }
+
+  def searchResults = Action {
+    val data = Form.
+    val results = ""
+    Ok(views.html.searchResults(results))
+  }
+
   def stations = Action {
     Ok(views.html.stations(planner.stations))
   }
-  
+
   def trips(name: String) = Action {
-    val station = name
     val hops = planner.hops.get(Station(name))
-    Ok(views.html.trips(station, hops))
-    
+    Ok(views.html.trips(name, hops))
   }
 
   def index = Action {
