125d5130e0cfbfbd4f9121932c46e1c03e880581
Hiren.Hiranandani <hhiranan@gmail.com>
2014-03-13 15:57:53 -0400

    Adding stations listing.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,8 +2,15 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import com.typesafe.training.scalatrain.Station
+import com.typesafe.training.scalatrain.TestData
 
 object Application extends Controller {
+  import TestData.planner
+
+  def stations = Action {
+    Ok(views.html.stations(planner.stations))
+  }
 
   def index = Action {
     Ok(views.html.index())
