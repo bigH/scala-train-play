5157796b5ace02b318c7f049560698057353d6ee
Hiren.Hiranandani <hhiranan@gmail.com>
2014-03-14 13:52:08 -0400

    Fixing integration tests.

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -6,19 +6,16 @@ import play.api.test._
 import play.api.test.Helpers._
 
 /**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
+* add your integration spec here.
+* An integration test will fire up a whole play application in a real (or headless) browser
+*/
 @RunWith(classOf[JUnitRunner])
 class IntegrationSpec extends Specification {
 
   "Application" should {
-
     "work from within a browser" in new WithBrowser {
-
       browser.goTo("http://localhost:" + port)
-
-      browser.pageSource must contain("Your new application is ready.")
+      browser.pageSource must contain("Monster Trains")
     }
   }
 }
