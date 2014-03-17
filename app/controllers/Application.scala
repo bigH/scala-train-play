d54ef79f86c1605a72fbf8668a9cc800f1f3a6cf
Greg Nieman <greg.nieman@gmail.com>
2014-03-17 15:46:18 -0400

    card 4

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,34 +9,36 @@ import play.api.data.Forms._
 object Application extends Controller {
   import TestData.planner
 
-  case class TripQuery(to: String, from: String)
-  
+  case class TripQuery(to: String = "Munich", from: String="Frankfurt", hours: Int = 0, minutes: Int = 0)
+
   val form = Form(
     mapping(
       "to" -> text,
-      "from" -> text
+      "from" -> text,
+      "hours" -> default(number(0,23),0),
+      "minutes" -> default(number(0,59),0)
     )(TripQuery.apply)(TripQuery.unapply)
   )
 
   def searchLoad = Action{
       Ok(views.html.search(form, planner.sortedStations))
-
-
   }
   
   def search = Action {
     implicit request =>
     form.bindFromRequest.fold(
       e=>BadRequest(views.html.search(e, planner.sortedStations)),
-      i=>Redirect(routes.Application.searchResults(i.from, i.to))
-
+      i=>Redirect(routes.Application.searchResults(i.from, i.to, i.hours, i.minutes))
     )
   }
 
-  def searchResults(from: String, to: String) = Action {
-    //
-    //val time = Option(Time(0,0))
-    val results = planner.connections(Station(from), Station(to), None)
+  def searchResults(from: String, to: String, hours: Int, minutes: Int) = Action {
+    Logger.error("Hours:" + hours + "  " + "Minutes:" + minutes)
+
+    val time = Some(Time(hours,minutes))
+
+    val results = planner.connections(Station(from), Station(to), time)
+
     Ok(views.html.searchResults(results))
   }
 
